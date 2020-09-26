af9e2722ed92b0ef57ef7de09864cdbb717692dd
ejveli <71405672+ejveli@users.noreply.github.com>
2020-09-26 20:21:39 +0100

    Update Question_5.java

--- Question_5.java
+++ Question_5.java
@@ -3,18 +3,19 @@ package assignment_3;
 public class Question_5 {
 
 	public static void main(String[] args) {
+		
+		int hours,minutes,seconds;
+		int inputSeconds=3695;
+		
+		seconds = inputSeconds % 60;
+		minutes = (inputSeconds % 3600)/60;
+		hours = inputSeconds / 3600;
+		
+		System.out.println("inputSecond is "+inputSeconds);
+		System.out.println(hours+" hours "+minutes+" minutes "+seconds+" seconds");
+		
 
-		
-		int hours,minutes,second;
-		int inputSecond=3695;
-		
-		hours=inputSecond/3600;
-		minutes=(inputSecond-hours*3600)/60;
-		second=((inputSecond-hours*3600-minutes*60))%60;
-		
-		System.out.println("inputSecond is "+inputSecond);
-		System.out.println(hours+" hours "+minutes+" minutes "+second+" seconds");
-		
 	}
+
 
 }
